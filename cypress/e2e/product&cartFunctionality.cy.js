643cfe989e8041fccbe7ad5d6901d22f3faf3fb1
Rebof <rebofkatwal7@gmail.com>
2025-05-24 19:23:59 +0545

    Cart functionality checked with quantity being 1

--- cypress/e2e/product&cartFunctionality.cy.js
+++ cypress/e2e/product&cartFunctionality.cy.js
@@ -1,66 +1,171 @@
 /// <reference types="cypress" />
 
 beforeEach(() => {
-  
+  cy.clearAllCookies();
   cy.clearAllLocalStorage();
-  cy.visit('/');
+  cy.clearAllSessionStorage();
+  cy.visit("/");
 
-  cy.get("a[href='/products']").click()
-  cy.url().should("include","/products")
-})
+  cy.get("a[href='/products']").click();
+  cy.url().should("include", "/products");
+});
 
-describe("Product and cart functionality check",()=>{
-    it("Verify All products and product detail page",()=>{
+describe("Product and cart functionality check", () => {
+  it("Verify All products and product detail page", () => {
+    cy.get(".features_items .col-sm-4")
+      .should("have.length.greaterThan", 0)
+      .each(($el) => {
+        cy.wrap($el).should("be.visible");
+      });
 
-        
-        cy.get('.features_items .col-sm-4')
-                                            .should('have.length.greaterThan', 0)
-                                            .each(($el) => {
-                                                cy.wrap($el).should('be.visible');
-        });
-        
-        cy.get("a[href='/product_details/1']").click()
+    cy.get("a[href='/product_details/1']").click();
 
-        cy.url().should("include","/product_details/1")
+    cy.url().should("include", "/product_details/1");
 
-        cy.get(':nth-child(5) > span').should("be.visible")
+    cy.get(":nth-child(5) > span").should("be.visible");
 
-        cy.get('.product-information > :nth-child(3)').should("be.visible")
+    cy.get(".product-information > :nth-child(3)").should("be.visible");
 
-        cy.get('.product-information > h2').should("be.visible")
+    cy.get(".product-information > h2").should("be.visible");
 
-        cy.get('.product-information > :nth-child(6)').should("be.visible")
+    cy.get(".product-information > :nth-child(6)").should("be.visible");
 
-        cy.get('.product-information > :nth-child(7)').should("be.visible")
+    cy.get(".product-information > :nth-child(7)").should("be.visible");
 
-        cy.get('.product-information > :nth-child(8)').should("be.visible")
-    })
+    cy.get(".product-information > :nth-child(8)").should("be.visible");
+  });
 
+  it("search for product", () => {
+    cy.get("#search_product").type("Blue");
 
-    it.only("search for product", ()=>{
+    cy.get("#submit_search").click();
 
-        cy.get('#search_product').type("Blue")
+    cy.get(".productinfo").should("contain", "Blue Top");
 
-        cy.get("#submit_search").click()
+    cy.get(".features_items .col-sm-4")
+      .should("have.length.greaterThan", 0)
+      .each(($el) => {
+        cy.wrap($el)
+          .should("be.visible")
+          .find(".productinfo")
+          .then(($info) => {
+            if ($info.text().includes("Blue Top")) {
+              cy.log("Found a product with Blue Top");
+            }
+          });
+      });
+  });
 
-        cy.get(".productinfo").should("contain", "Blue Top")
+  it.only("Adding product to the cart and verifying total price", () => {
+  const quantity = 1;
 
-        
+  let firstProdName, secondProdName;
+  let firstProdPrice, secondProdPrice;
+  let cartFirstProdName, cartSecondProdName;
+  let cartFirstProdPrice, cartSecondProdPrice;
+  let cartFirstProdTotal, cartSecondProdTotal;
 
-        cy.get('.features_items .col-sm-4')
-            .should('have.length.greaterThan', 0)
-            .each(($el) => {
-                cy.wrap($el)
-                    .should('be.visible')
-                    .find('.productinfo')
-                    .then(($info) => {
-                        if ($info.text().includes('Blue Top')) {
-                            cy.log('Found a product with Blue Top');
-                        }
-                    });
-                });
-     
-    })
+  // Get first product info 
+  cy.get(".features_items .col-sm-4").eq(0).within(() => {
+    cy.get(".productinfo p")
+      .invoke("text")
+      .then((text) => {
+        firstProdName = text.trim();
+      });
+    cy.get(".productinfo h2")
+      .invoke("text")
+      .then((text) => {
+        firstProdPrice = (text.replace("$", "").trim());
+      });
+    cy.get(".add-to-cart").first().click();
+  });
 
+  cy.get(".modal-footer > .btn").click();
 
-})
+  // Get second product info
+  cy.get(".features_items .col-sm-4").eq(2).within(() => {
+    cy.get(".productinfo p")
+      .invoke("text")
+      .then((text) => {
+        secondProdName = text.trim();
+      });
+    cy.get(".productinfo h2")
+      .invoke("text")
+      .then((text) => {
+        secondProdPrice = (text.replace("$", "").trim());
+      });
+    cy.get(".add-to-cart").first().click();
+  });
+
+  cy.get(".modal-footer > .btn").click();
+
+  // Go to cart page
+  cy.get(".shop-menu > .nav > :nth-child(3) > a").click();
+  cy.url().should("include", "/view_cart");
+
+  // Get first product cart
+  cy.get("#product-1").within(() => {
+    cy.get(".cart_description h4 a")
+      .invoke("text")
+      .then((text) => {
+        cartFirstProdName = text.trim();
+      });
+
+    cy.get(".cart_price p")
+      .invoke("text")
+      .then((text) => {
+        cartFirstProdPrice = (text.replace("$", "").trim());
+      });
+
+    cy.get(".cart_total .cart_total_price")
+      .invoke("text")
+      .then((text) => {
+        cartFirstProdTotal = (text.replace("$", "").trim());
+      });
+  });
+
+  // Get second produc cart
+  cy.get("#product-3").within(() => {
+    cy.get(".cart_description h4 a")
+      .invoke("text")
+      .then((text) => {
+        cartSecondProdName = text.trim();
+      });
+
+    cy.get(".cart_price p")
+      .invoke("text")
+      .then((text) => {
+        cartSecondProdPrice = (text.replace("$", "").trim());
+      });
+
+    cy.get(".cart_total .cart_total_price")
+      .invoke("text")
+      .then((text) => {
+        cartSecondProdTotal = (text.replace("$", "").trim());
+      });
+  });
+
+  // Compare product matches with cart info or not
+  cy.then(() => {
+    // Name checks
+    expect(cartFirstProdName).to.eq(firstProdName);
+    expect(cartSecondProdName).to.eq(secondProdName);
+
+    expect(cartFirstProdPrice).to.eq(firstProdPrice);
+    expect(cartSecondProdPrice).to.eq(secondProdPrice);
+
+    const parsedFirstProdPrice = parseFloat((firstProdPrice + '').replace(/[^0-9.]/g, ''));
+    const parsedSecondProdPrice = parseFloat((secondProdPrice + '').replace(/[^0-9.]/g, ''));
+    const parsedCartFirstProdTotal = parseFloat((cartFirstProdTotal + '').replace(/[^0-9.]/g, ''));
+    const parsedCartSecondProdTotal = parseFloat((cartSecondProdTotal + '').replace(/[^0-9.]/g, ''));
+    
+    // Total price checks
+    expect(parsedCartFirstProdTotal).to.eq(parsedFirstProdPrice * quantity);
+    expect(parsedCartSecondProdTotal).to.eq(parsedSecondProdPrice * quantity);
+
+    cy.log("First product matches in cart including total price");
+    cy.log("Second product matches in cart including total price");
+  });
+});
+
+});
