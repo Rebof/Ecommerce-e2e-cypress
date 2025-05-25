9e12bb8d4c4329c91964d9d60caf044a26151f5f
Rebof <rebofkatwal7@gmail.com>
2025-05-25 13:09:36 +0545

    Custom cmds added

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,6 +25,8 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 
+import { faker } from "@faker-js/faker";
+
 Cypress.Commands.add("deleteAccount", ()=>{
     cy.visit("/")
     cy.get("a[href='/delete_account']").should("be.visible").click()
@@ -69,3 +71,81 @@ Cypress.Commands.add("createUserWithApi",(user)=>{
         
     })
 })
+
+
+
+Cypress.Commands.add("registerUser",(user)=>{
+
+    cy.get(".signup-form > h2").should("contain", "New User Signup!");
+    cy.get("input[placeholder='Name']").type(user.name);
+    cy.get("input[data-qa='signup-email']").type(`${user.email}{enter}`);
+
+    cy.get(":nth-child(1) > b").should(
+      "have.text",
+      "Enter Account Information"
+    );
+
+    cy.get("#id_gender1").check().should("be.visible");
+    cy.get('[data-qa="password"]').type(user.password);
+
+    cy.get("#days").type("30");
+    cy.get("#months").type("10");
+    cy.get("#years").type("2000");
+
+    cy.get("#first_name").type(user.firstname);
+    cy.get("#last_name").type(user.lastname);
+
+    cy.get("#address1").type(user.address1);
+    cy.get("#address2").type(user.address2);
+
+    cy.get("#country")
+      .select(user.country)
+      .should("have.value", "United States");
+    cy.get("#state").type(user.state);
+    cy.get("#city").type(user.city);
+    cy.get("#zipcode").type(user.zipcode);
+
+    cy.get("#mobile_number").type(user.mobile_number).type("{enter}");
+
+    cy.get("b").should("have.text", "Account Created!");
+
+    cy.get('[data-qa="continue-button"]').click(); 
+
+    cy.get("li:nth-child(10) a:nth-child(1)").should(
+      "contain",
+      `Logged in as ${user.name}`
+    ); 
+    
+})
+
+
+Cypress.Commands.add("addtocart",()=>{
+
+    cy.get("a[href='/products']").click();
+    cy.url().should("include", "/products");
+    cy.get(".add-to-cart").first().click();
+    cy.get(".modal-footer > .btn").click();
+    cy.get(".shop-menu > .nav > :nth-child(3) > a").click();
+    cy.url().should("include", "/view_cart");
+    cy.get(".btn.btn-default.check_out").click()
+    
+})
+
+
+Cypress.Commands.add("verifyAddress", (id, user) => {
+  cy.get(id).within(() => {
+    cy.get(".address_address1").should("contain", user.address1);
+    cy.get('.address_city').should("contain", user.city);
+    cy.get('.address_country_name').should("contain", user.country);
+  });
+});
+
+
+Cypress.Commands.add("enterPaymentDetails", () => {
+  cy.get('[data-qa="name-on-card"]').type("Sanima Bank's Card");
+  cy.get('[data-qa="card-number"]').type("12345");
+  cy.get('[data-qa="cvc"]').type("4");
+  cy.get('[data-qa="expiry-month"]').type("DD");
+  cy.get('[data-qa="expiry-year"]').type("2030");
+  cy.get('[data-qa="pay-button"]').click();
+});
